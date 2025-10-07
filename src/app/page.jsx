8796fb9928f1ecd87f283b050f5220129501cae2
gabgon08 <gabriel.goncalves08@outlook.com>
2025-10-07 00:38:48 -0300

    feat: Header and Footer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import styles from './page.module.css'
+import Header from '../components/Header'
 
 function Home() {
   return (
-    <div>
-      <p>The Fate of Ophelia</p>
-      <p>Actually Romantic</p>
-      <p>Elizabeth Taylor</p>
+    <div className={styles.container}>
+      <Header />
+
+      <div className={styles.main}>
+        <p>The Fate of Ophelia</p>
+        <p>Actually Romantic</p>
+        <p>Elizabeth Taylor</p>
+      </div>
+
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+function Header() {
+    return (
+        <div className={styles.container}>The Life of a Showgirl
+
+        </div>
+    )
+}
+
+export default Header
