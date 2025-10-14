4af6f6f1d6e58448ed18aa34c0484e4f8d74dba1
gabgon08 <gabriel.goncalves08@outlook.com>
2025-10-14 19:08:48 -0300

    feat: franquia and funcionarios get tela

---
+++ src/app/franquias/page.jsx
@@ -0,0 +1,65 @@
+'use client'
+
+import React from 'react'
+
+import { useState, useEffect } from 'react';
+import { Table } from 'antd';
+
+function Franquias() {
+
+    //Criar uma variavel para armazenar a lista de franquias
+    const [franquias, setFranquias] = useState([]);
+
+    async function carregarFranquias() {
+        try {
+            const response = await fetch('/api/franquias')
+            const data = await response.json()
+            setFranquias(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        carregarFranquias();
+    }, [])
+
+    //Table do antd
+
+
+    const columns = [
+        {
+            title: 'Nome',
+            dataIndex: 'nome',
+            key: 'id'
+        },
+        {
+            title: 'Cidade',
+            dataIndex: 'cidade',
+            key: 'id'
+        },
+        {
+            title: 'Endereço',
+            dataIndex: 'endereco',
+            key: 'id'
+        },
+        {
+            title: 'Telefone',
+            dataIndex: 'telefone',
+            key: 'id'
+        }
+    ]
+
+    return (
+        <div>
+            <h1>Franquias</h1>
+            <Table
+                columns={columns}
+                dataSource={franquias}
+                rowKey='id'
+            />
+        </div>
+    )
+}
+
+export default Franquias
---
+++ src/app/funcionarios/page.jsx
@@ -0,0 +1,75 @@
+'use client'
+
+import React from 'react'
+
+import { useState, useEffect } from 'react';
+import { Table } from 'antd';
+
+function Funcionarios() {
+
+    //Criar uma variavel para armazenar a lista de franquias
+    const [funcionarios, setFuncionarios] = useState([]);
+
+    async function carregarFuncionarios() {
+        try {
+            const response = await fetch('/api/funcionarios')
+            const data = await response.json()
+            setFuncionarios(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        carregarFuncionarios();
+    }, [])
+
+    //Table do antd
+
+
+    const columns = [
+        {
+            title: 'Nome',
+            dataIndex: 'nome',
+            key: 'id'
+        },
+        {
+            title: 'E-mail',
+            dataIndex: 'email',
+            key: 'id'
+        },
+        {
+            title: 'Cargo',
+            dataIndex: 'cargo',
+            key: 'id'
+        },
+        {
+            title: 'Salário',
+            dataIndex: 'salario',
+            key: 'id',
+            render: (valor) =>
+                valor.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                }),
+        },
+        {
+            title: 'Franquia',
+            dataIndex: ['franquia', 'nome'],
+            key: 'id'
+        }
+    ]
+
+    return (
+        <div>
+            <h1>Funcionários</h1>
+            <Table
+                columns={columns}
+                dataSource={funcionarios}
+                rowKey='id'
+            />
+        </div>
+    )
+}
+
+export default Funcionarios
