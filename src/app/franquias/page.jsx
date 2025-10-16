a8b6af7f670e352d76463635977a10ea98ecc9b3
gabgon08 <gabriel.goncalves08@outlook.com>
2025-10-16 19:26:00 -0300

    feat: franquias page

--- src/app/franquias/page.jsx
+++ src/app/franquias/page.jsx
@@ -1,63 +1,27 @@
 'use client'
 
-import React from 'react'
-
-import { useState, useEffect } from 'react';
-import { Table } from 'antd';
+import React, { useState, useEffect } from 'react'
+import styles from './franquias.module.css'
 
 function Franquias() {
 
-    //Criar uma variavel para armazenar a lista de franquias
-    const [franquias, setFranquias] = useState([]);
+    const [franquias, setFranquias] = useState([])
 
-    async function carregarFranquias() {
-        try {
-            const response = await fetch('/api/franquias')
-            const data = await response.json()
-            setFranquias(data);
-        } catch (error) {
-            console.error(error);
-        }
+    const [loading, setLoading] = useState(true)
+
+    async function CarregarFranquias(params) {
+        console.log('BUSCAR FRANQUIAS')
+        setLoading(false)
+
+        useEffect(() => {
+            CarregarFranquias()
+        }, [])
     }
 
-    useEffect(() => {
-        carregarFranquias();
-    }, [])
-
-    //Table do antd
-
-
-    const columns = [
-        {
-            title: 'Nome',
-            dataIndex: 'nome',
-            key: 'id'
-        },
-        {
-            title: 'Cidade',
-            dataIndex: 'cidade',
-            key: 'id'
-        },
-        {
-            title: 'Endereço',
-            dataIndex: 'endereco',
-            key: 'id'
-        },
-        {
-            title: 'Telefone',
-            dataIndex: 'telefone',
-            key: 'id'
-        }
-    ]
-
     return (
-        <div>
-            <h1>Franquias</h1>
-            <Table
-                columns={columns}
-                dataSource={franquias}
-                rowKey='id'
-            />
+        <div className={styles.main}>
+            <h1>FRANQUIAS</h1>
+            <p>{JSON.stringify(franquias)}</p>
         </div>
     )
 }
