cecdb1cab4dc2fa66d91b21d81b56a5cfb34ee02
gabgon08 <gabriel.goncalves08@outlook.com>
2025-10-17 07:10:04 -0300

    feat: update funcionarios page

--- src/app/funcionarios/page.jsx
+++ src/app/funcionarios/page.jsx
@@ -1,27 +1,50 @@
 'use client'
 
-import React from 'react'
-
-import { useState, useEffect } from 'react';
-import { Table } from 'antd';
+import React, { useState, useEffect } from 'react'
+import styles from './funcionarios.module.css'
+import MainTheme from '@/theme'
+import { Table, Button, Modal, Form, message, Input, Typography, InputNumber, Select, Space, Popconfirm } from 'antd'
+import { PlusOutlined, UserOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons'
 
 function Funcionarios() {
 
-    //Criar uma variavel para armazenar a lista de franquias
-    const [funcionarios, setFuncionarios] = useState([]);
+    const { Title, Paragraph } = Typography
+    const [funcionarios, setFuncionarios] = useState([])
+    const [franquias, setFranquias] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [modalVisible, setModalVisible] = useState(false)
+    const [editandoId, setEditandoId] = useState(null)
+    const [form] = Form.useForm()
+    const [messageApi, contextHolder] = message.useMessage()
+
 
     async function carregarFuncionarios() {
         try {
+            setLoading(true)
             const response = await fetch('/api/funcionarios')
             const data = await response.json()
             setFuncionarios(data);
         } catch (error) {
-            console.error(error);
+            messageApi.error('Erro ao carregar funcionários')
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    async function carregarFranquias() {
+        try {
+            const response = await fetch('/api/franquias')
+            const data = await response.json()
+            setFranquias(data)
+
+        } catch (error) {
+            messageApi.error('Erro ao carregar franquias')
         }
     }
 
     useEffect(() => {
-        carregarFuncionarios();
+        carregarFuncionarios()
+        carregarFranquias()
     }, [])
 
     //Table do antd
@@ -62,14 +85,34 @@ function Funcionarios() {
     ]
 
     return (
-        <div>
-            <h1>Funcionários</h1>
-            <Table
-                columns={columns}
-                dataSource={funcionarios}
-                rowKey='id'
-            />
-        </div>
+        <MainTheme>
+            <div className={styles.container}>
+                <div className={styles.top}>
+                    <Title
+                        level={2}
+                        style={{
+                            fontWeight: 'bold'
+                        }}
+                    >
+                        FUNCIONÁRIOS
+                    </Title>
+
+                    <Button
+                        type='primary'
+                        icon={<PlusOutlined />}
+                        className={styles.addButton}
+                    >
+                        Adicionar
+                    </Button>
+                </div>
+
+                <Table
+                    columns={columns}
+                    dataSource={funcionarios}
+                    rowKey='id'
+                />
+            </div>
+        </MainTheme>
     )
 }
 
