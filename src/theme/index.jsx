77389243c5b5464161c3d09043100f4cd877603c
gabgon08 <gabriel.goncalves08@outlook.com>
2025-10-17 04:48:37 -0300

    style: update franquias page

--- src/theme/index.jsx
+++ src/theme/index.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import { ConfigProvider } from 'antd'
+import { volcano } from '@ant-design/colors'
 
 const MainTheme = ({ children }) => {
   const primaryColorCode = '#35725f'
-  const secondaryColor = '#22493dff'
+  const textColor = '#22493dff'
+  const headerBgColor = '#35725f'
+  const headerTextColor = '#e2fffa'
+  const tableBgColor = 'rgba(255, 255, 255, 0)'
 
   return (
     <ConfigProvider
       theme={{
         token: {
           colorPrimary: primaryColorCode,
-          colorTextBase: secondaryColor,
+          colorTextBase: textColor,
         },
+        components: {
+          Table: {
+            colorBgContainer: tableBgColor,
+            headerBg: headerBgColor,
+            headerColor: headerTextColor,
+            rowHoverBg: '#35725f2a',
+            rowExpandedBg: '#35725f2a'
+          },
+        }
       }}
     >
       {children}
@@ -23,15 +36,20 @@ export default MainTheme
 
 export const HeaderTheme = ({ children }) => {
   const primaryColorCode = '#6e290f'
-  const secondaryColor = '#ffffffff'
+  const textColor = '#ffffffff'
 
   return (
     <ConfigProvider
       theme={{
         token: {
           colorPrimary: primaryColorCode,
-          colorTextBase: secondaryColor
+          colorTextBase: textColor
         },
+        components: {
+          Menu: {
+            // activeBarHeight: 2
+          }
+        }
       }}
     >
       {children}
