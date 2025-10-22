507f255cd69154305553f3467eb66bc298b552f0
gabgon08 <gabriel.goncalves08@outlook.com>
2025-10-22 05:51:51 -0300

    refactor: update Home layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,18 +1,30 @@
+'use client'
+
 import React from 'react'
 import styles from './page.module.css'
 import common from './../theme/common.module.css'
+import { LayoutTheme } from './../theme/index'
+import { Layout } from 'antd'
 
 function Home() {
+
+  const { Content } = Layout
+  const { token } = theme.useToken()
+
   return (
-    <div className={common.container}>
+    <LayoutTheme>
+      <Layout className={common.layout}>
+        <Content className={common.container}>
 
-      <div className={styles.main}>
-        <p>The Fate of Ophelia</p>
-        <p>Actually Romantic</p>
-        <p>Elizabeth Taylor</p>
-      </div>
+          <div className={styles.main}>
+            <p>The Fate of Ophelia</p>
+            <p>Actually Romantic</p>
+            <p>Elizabeth Taylor</p>
+          </div>
 
-    </div>
+        </Content>
+      </Layout>
+    </LayoutTheme>
   )
 }
 
