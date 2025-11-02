70e014a98d926d61d3368c8c4cfe54f5c354396f
gabgon08 <gabriel.goncalves08@outlook.com>
2025-11-02 18:52:05 -0300

    Reapply "style: change font family"
    This reverts commit a20d15da1ca2314845a8f8c843158d9b54c3b2d8.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Quicksand } from "next/font/google";
 import "./globals.css";
 import Header from '../components/Header'
 import { LayoutTheme } from "./../theme/index.jsx";
@@ -6,15 +6,7 @@ import { ConfigProvider } from "antd";
 import '@ant-design/v5-patch-for-react-19';
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const quickFont = Quicksand({ subsets: ['latin'], weight: '500' })
 
 export const metadata = {
   title: "SGF",
@@ -25,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <ConfigProvider>
       <html lang="pt-BR">
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <body className={quickFont.className}>
           <Header />
           <LayoutTheme>
             {children}
--- src/theme/index.jsx
+++ src/theme/index.jsx
@@ -36,6 +36,7 @@ export function LayoutTheme({ children }) {
 
       theme={{
         token: {
+          fontFamily: 'var(--font-principal)',
           colorPrimary: colors.steel[1],
           colorText: colors.steel[1],
           colorBgContainer: colors.steel[7],
@@ -94,6 +95,7 @@ export function HeaderTheme({ children }) {
     <ConfigProvider
       theme={{
         token: {
+          fontFamily: 'var(--font-principal)',
           colorPrimary: colors.brown[2],
           colorText: colors.brown[7],
         },
